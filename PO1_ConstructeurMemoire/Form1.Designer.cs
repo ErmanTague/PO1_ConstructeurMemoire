75aa231f4db021bc0ef156c79b091c5ff3fa27c6
ErmanTague <2432311@cegepba.qc.ca>
2025-02-20 17:32:19 +0100

    Programme terminé

--- PO1_ConstructeurMemoire/Form1.Designer.cs
+++ PO1_ConstructeurMemoire/Form1.Designer.cs
@@ -52,23 +52,27 @@
             // listBox_joueurs
             // 
             listBox_joueurs.FormattingEnabled = true;
-            listBox_joueurs.Location = new Point(16, 26);
+            listBox_joueurs.ItemHeight = 37;
+            listBox_joueurs.Location = new Point(30, 48);
+            listBox_joueurs.Margin = new Padding(6, 6, 6, 6);
             listBox_joueurs.Name = "listBox_joueurs";
-            listBox_joueurs.Size = new Size(230, 104);
+            listBox_joueurs.Size = new Size(428, 189);
             listBox_joueurs.TabIndex = 1;
             // 
             // textBox_ajouterJoueurNom
             // 
-            textBox_ajouterJoueurNom.Location = new Point(339, 26);
+            textBox_ajouterJoueurNom.Location = new Point(636, 48);
+            textBox_ajouterJoueurNom.Margin = new Padding(6, 6, 6, 6);
             textBox_ajouterJoueurNom.Name = "textBox_ajouterJoueurNom";
-            textBox_ajouterJoueurNom.Size = new Size(151, 27);
+            textBox_ajouterJoueurNom.Size = new Size(280, 43);
             textBox_ajouterJoueurNom.TabIndex = 2;
             // 
             // button_ajouterJoueur
             // 
-            button_ajouterJoueur.Location = new Point(339, 93);
+            button_ajouterJoueur.Location = new Point(636, 172);
+            button_ajouterJoueur.Margin = new Padding(6, 6, 6, 6);
             button_ajouterJoueur.Name = "button_ajouterJoueur";
-            button_ajouterJoueur.Size = new Size(94, 29);
+            button_ajouterJoueur.Size = new Size(176, 54);
             button_ajouterJoueur.TabIndex = 3;
             button_ajouterJoueur.Text = "Ajouter";
             button_ajouterJoueur.UseVisualStyleBackColor = true;
@@ -77,35 +81,40 @@
             // label_ajouterJoueurNom
             // 
             label_ajouterJoueurNom.AutoSize = true;
-            label_ajouterJoueurNom.Location = new Point(267, 29);
+            label_ajouterJoueurNom.Location = new Point(501, 54);
+            label_ajouterJoueurNom.Margin = new Padding(6, 0, 6, 0);
             label_ajouterJoueurNom.Name = "label_ajouterJoueurNom";
-            label_ajouterJoueurNom.Size = new Size(49, 20);
+            label_ajouterJoueurNom.Size = new Size(89, 37);
             label_ajouterJoueurNom.TabIndex = 4;
             label_ajouterJoueurNom.Text = "Nom :";
             // 
             // comboBox_ajouterJoueurEquipe
             // 
             comboBox_ajouterJoueurEquipe.FormattingEnabled = true;
-            comboBox_ajouterJoueurEquipe.Location = new Point(339, 59);
+            comboBox_ajouterJoueurEquipe.Location = new Point(636, 109);
+            comboBox_ajouterJoueurEquipe.Margin = new Padding(6, 6, 6, 6);
             comboBox_ajouterJoueurEquipe.Name = "comboBox_ajouterJoueurEquipe";
-            comboBox_ajouterJoueurEquipe.Size = new Size(151, 28);
+            comboBox_ajouterJoueurEquipe.Size = new Size(280, 45);
             comboBox_ajouterJoueurEquipe.TabIndex = 5;
             // 
             // label_ajouterJoueurEquipe
             // 
             label_ajouterJoueurEquipe.AutoSize = true;
-            label_ajouterJoueurEquipe.Location = new Point(267, 62);
+            label_ajouterJoueurEquipe.Location = new Point(501, 115);
+            label_ajouterJoueurEquipe.Margin = new Padding(6, 0, 6, 0);
             label_ajouterJoueurEquipe.Name = "label_ajouterJoueurEquipe";
-            label_ajouterJoueurEquipe.Size = new Size(66, 20);
+            label_ajouterJoueurEquipe.Size = new Size(119, 37);
             label_ajouterJoueurEquipe.TabIndex = 6;
             label_ajouterJoueurEquipe.Text = "Équipe : ";
             // 
-            // listBox_equipe
+            // listBox_equipes
             // 
             listBox_equipes.FormattingEnabled = true;
-            listBox_equipes.Location = new Point(16, 26);
-            listBox_equipes.Name = "listBox_equipe";
-            listBox_equipes.Size = new Size(230, 104);
+            listBox_equipes.ItemHeight = 37;
+            listBox_equipes.Location = new Point(30, 48);
+            listBox_equipes.Margin = new Padding(6, 6, 6, 6);
+            listBox_equipes.Name = "listBox_equipes";
+            listBox_equipes.Size = new Size(428, 189);
             listBox_equipes.TabIndex = 7;
             // 
             // groupBox_Equipe
@@ -114,18 +123,22 @@
             groupBox_Equipe.Controls.Add(label_modifierEquipeNom);
             groupBox_Equipe.Controls.Add(listBox_equipes);
             groupBox_Equipe.Controls.Add(textBox_modifierEquipeNom);
-            groupBox_Equipe.Location = new Point(25, 159);
+            groupBox_Equipe.Location = new Point(47, 294);
+            groupBox_Equipe.Margin = new Padding(6, 6, 6, 6);
             groupBox_Equipe.Name = "groupBox_Equipe";
-            groupBox_Equipe.Size = new Size(595, 162);
+            groupBox_Equipe.Padding = new Padding(6, 6, 6, 6);
+            groupBox_Equipe.Size = new Size(1116, 300);
             groupBox_Equipe.TabIndex = 8;
             groupBox_Equipe.TabStop = false;
             groupBox_Equipe.Text = "Équipes";
+            groupBox_Equipe.Enter += groupBox_Equipe_Enter;
             // 
             // button_modifierEquipeNom
             // 
-            button_modifierEquipeNom.Location = new Point(339, 59);
+            button_modifierEquipeNom.Location = new Point(636, 109);
+            button_modifierEquipeNom.Margin = new Padding(6, 6, 6, 6);
             button_modifierEquipeNom.Name = "button_modifierEquipeNom";
-            button_modifierEquipeNom.Size = new Size(94, 29);
+            button_modifierEquipeNom.Size = new Size(176, 54);
             button_modifierEquipeNom.TabIndex = 9;
             button_modifierEquipeNom.Text = "Modifier";
             button_modifierEquipeNom.UseVisualStyleBackColor = true;
@@ -134,17 +147,19 @@
             // label_modifierEquipeNom
             // 
             label_modifierEquipeNom.AutoSize = true;
-            label_modifierEquipeNom.Location = new Point(279, 33);
+            label_modifierEquipeNom.Location = new Point(523, 61);
+            label_modifierEquipeNom.Margin = new Padding(6, 0, 6, 0);
             label_modifierEquipeNom.Name = "label_modifierEquipeNom";
-            label_modifierEquipeNom.Size = new Size(49, 20);
+            label_modifierEquipeNom.Size = new Size(89, 37);
             label_modifierEquipeNom.TabIndex = 10;
             label_modifierEquipeNom.Text = "Nom :";
             // 
             // textBox_modifierEquipeNom
             // 
-            textBox_modifierEquipeNom.Location = new Point(339, 26);
+            textBox_modifierEquipeNom.Location = new Point(636, 48);
+            textBox_modifierEquipeNom.Margin = new Padding(6, 6, 6, 6);
             textBox_modifierEquipeNom.Name = "textBox_modifierEquipeNom";
-            textBox_modifierEquipeNom.Size = new Size(151, 27);
+            textBox_modifierEquipeNom.Size = new Size(280, 43);
             textBox_modifierEquipeNom.TabIndex = 9;
             // 
             // groupBox_joueur
@@ -155,9 +170,11 @@
             groupBox_joueur.Controls.Add(comboBox_ajouterJoueurEquipe);
             groupBox_joueur.Controls.Add(button_ajouterJoueur);
             groupBox_joueur.Controls.Add(label_ajouterJoueurNom);
-            groupBox_joueur.Location = new Point(25, 12);
+            groupBox_joueur.Location = new Point(47, 22);
+            groupBox_joueur.Margin = new Padding(6, 6, 6, 6);
             groupBox_joueur.Name = "groupBox_joueur";
-            groupBox_joueur.Size = new Size(595, 141);
+            groupBox_joueur.Padding = new Padding(6, 6, 6, 6);
+            groupBox_joueur.Size = new Size(1116, 261);
             groupBox_joueur.TabIndex = 11;
             groupBox_joueur.TabStop = false;
             groupBox_joueur.Text = "Joueurs";
@@ -172,11 +189,12 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(15F, 37F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(672, 353);
+            ClientSize = new Size(1260, 653);
             Controls.Add(groupBox_joueur);
             Controls.Add(groupBox_Equipe);
+            Margin = new Padding(6, 6, 6, 6);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
